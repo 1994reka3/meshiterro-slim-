664f4895cc3d0c4f04d9f3c173caa37dd29e3f6b
vagrant <vagrant@localhost.localdomain>
2020-12-12 02:46:01 +0000

    [update]showページ作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b07482a47a112aa2915b3064d53341ac03b3e9d8594069a3adef1ada831d81a73c4189ccbea83e0ab81aaae70a5520a78e5b8ae95cd25a4d6c6d950911a2e86d'
